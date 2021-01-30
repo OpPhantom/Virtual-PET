bd29410e021d738f4711efb53dba1e173e1c0b4a
OpPhantom <71213957+OpPhantom@users.noreply.github.com>
2021-01-30 14:28:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var  dog, happyDog;
+
+var database;
+var foodS, foodStock;
+
+function preload()
+{
+  dogImg = loadImage("images/Dog.png");
+  dogImg1 = loadImage("images/happydog.png");
+}
+
+function setup() {
+  createCanvas(500,500);
+  
+  dog = createSprite(250,250,10,10);
+  dog.addImage(dogImg);
+  dog.scale = 0.4;
+  
+  database = firebase.database();
+
+  foodStock = database.ref('Food');
+  foodStock.on("value", readStock);
+
+    
+}
+
+
+function draw() {  
+  background("lightGreen");
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogImg1);
+  }
+
+  drawSprites();
+
+  text("Food Remaining:" + foodS, 250,480);
+}
+
+function readStock(data){
+  foodS=data.val();
+}
+
+function writeStock (x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  }
+
+  database.ref('/').update({
+    Food: x 
+  })
+}
